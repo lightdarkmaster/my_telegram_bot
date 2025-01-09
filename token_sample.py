c4b1c702f0b29b4da22041a799a664dbbbe83ec3
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2025-01-10 00:04:18 +0800

    Add Sample Token

---
+++ token_sample.py
@@ -0,0 +1,6 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+
+TOKEN: Final = 'YOUR_BOT_TOKEN_HERE'
+BOT_USERNAME: Final = '@BOT_USERNAME_HERE'
