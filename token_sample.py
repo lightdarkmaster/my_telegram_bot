48f5dc366f78325522fd157abcfba9ba3808112b
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2025-01-11 00:06:54 +0800

    Add Guide Comments

--- token_sample.py
+++ token_sample.py
@@ -4,3 +4,6 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 
 TOKEN: Final = 'YOUR_BOT_TOKEN_HERE'
 BOT_USERNAME: Final = '@BOT_USERNAME_HERE'
+
+## Replace the bot_token by the Telegram API 
+## and the username of the bot
